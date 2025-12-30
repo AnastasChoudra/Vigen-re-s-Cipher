d793aa6f7130fc90bffb686cefdc4c0c2f5ca510
AnastasChoudra <168728860+AnastasChoudra@users.noreply.github.com>
2025-12-30 15:29:10 +0000

    corrected wrong identation

--- Vigenere's Cipherpy.py
+++ Vigenere's Cipherpy.py
@@ -11,18 +11,18 @@ def vigenere_decode(message, keyword):
             keyword_phrase += keyword[keyword_index]
             keyword_index += 1
         else:
-            keyword_phrase += character
+            keyword_phrase += character 
     decoded_message = ""
 
     for i in range(len(message)):
         if message[i] in alphabet:
             old_character_index = alphabet.find(message[i])
             offset_index = alphabet.find(keyword_phrase[i])
-            new_character = alphabet[(old_character_index - offset_index) % 26]
+            new_character = alphabet[(old_character_index + offset_index) % 26]
             decoded_message += new_character
-    else:
+        else:
             decoded_message += message[i]
-    
+        
     return decoded_message
 
 vigenere_message = "txm srom vkda gl lzlgzr qpdb? fepb ejac! ubr imn tapludwy mhfbz cza ruxzal wg zztcgcexxch!"
@@ -41,19 +41,18 @@ def vigenere_encode(message, keyword):
             keyword_phrase += keyword[keyword_index]
             keyword_index += 1
         else:
-            keyword_phrase += character
-
+            keyword_phrase += character 
     encoded_message = ""
 
     for i in range(len(message)):
         if message[i] in alphabet:
             old_character_index = alphabet.find(message[i])
             offset_index = alphabet.find(keyword_phrase[i])
-            new_character = alphabet[(old_character_index + offset_index) % 26]
+            new_character = alphabet[(old_character_index - offset_index) % 26]
             encoded_message += new_character
         else:
             encoded_message += message[i]
-
+    
     return encoded_message
 
 vigenere_message_for_v = "thanks for teaching me all these cool ciphers! you really are the best!"
